be1bca1f69ab738ce309bfdfebddf06e82ff3494
etiennezastko <etiennezastko@hotmail.fr>
2021-04-02 11:41:37 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const idSquare = document.getElementById('animSquare');
+var count = 0;
+console.log(idSquare);
+idSquare.addEventListener('click',function(){
+    if (count == 0) {
+        idSquare.style.backgroundColor = 'red';
+        count = 1;
+    } else if (count == 1){
+        idSquare.style.backgroundColor = 'green';
+        count = 2;
+    } else {
+        idSquare.style.backgroungColor = 'blue' ;
+        count = 0;
+    } 
+    
+});
